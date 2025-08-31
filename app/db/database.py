b8ddb9a71cb01aec26084801dc0c98c028f6f630
Eren Sonmez <59799188+erensonmezx@users.noreply.github.com>
2025-08-31 21:58:15 +0300

    refactor: centralize SQLAlchemy Base

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,7 @@
 # app/db/database.py
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
